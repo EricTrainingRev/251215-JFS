ca6456c02b9f2d07e5ada712428614f8b1a17b16
EricTrainingRev <eric.suminski@revature.com>
2025-12-18 09:57:33 -0500

    adding initial ts stuff

---
+++ typescript/index.js
@@ -0,0 +1,6 @@
+/*
+    If you want to execute your code with node you simply reference it after the node command. Keep
+    in mind if working with ts you can't directly execute your ts code: you must first use the
+    TypeScript compiler (tsc) to compile your code from ts into js, then you can execute it
+*/
+console.log("Hello world!");
---
+++ typescript/index.ts
@@ -0,0 +1,11 @@
+/*
+    If you want to execute your code with node you simply reference it after the node command. Keep
+    in mind if working with ts you can't directly execute your ts code: you must first use the
+    TypeScript compiler (tsc) to compile your code from ts into js, then you can execute it
+
+    step 1 -> write your TypeScript code
+    step 2 -> compile your TypeScript code using npx tsc
+    step 3 -> execute your newly compiled JavaScript code
+*/
+
+console.log("Hello world!");
