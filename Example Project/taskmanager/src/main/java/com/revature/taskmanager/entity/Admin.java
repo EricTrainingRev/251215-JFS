9b7a74b5c41c6b9b67310a230e824c32eb6b530f
EricTrainingRev <eric.suminski@revature.com>
2025-12-18 12:44:20 -0500

    Adding Lombok utilizing Admin base entity with no Spring integration yet

---
+++ Example Project/taskmanager/src/main/java/com/revature/taskmanager/entity/Admin.java
@@ -0,0 +1,31 @@
+package com.revature.taskmanager.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/*
+    Here we are going to use lombok to generate our Java Bean resources:
+    - getter and setter methods for each field
+    - no args and full args constructors
+    - overridden toString, equals, and hashcode
+
+    Note that we had to include the NoArgsConstructor annotation to also get the
+    no args constructor
+ */
+@Data
+@NoArgsConstructor
+public class Admin {
+
+    /*
+        UUID is a randomly generated value that can be used to identify an entity. The
+        odds of two UUIDs being the same are astronomically low, so it is a reasonable
+        option when you need to generate unique identifiers
+     */
+
+    private UUID adminID;
+    private String username;
+    private String password;
+
+}
