c4035ff35aebef93578516fac1da38eb2abb7f97
EricTrainingRev <eric.suminski@revature.com>
2025-12-23 10:51:26 -0500

    adding initial sprite implementation remembering to use signal

--- angular/pokedex/src/app/components/poke-sprites/poke-sprites.ts
+++ angular/pokedex/src/app/components/poke-sprites/poke-sprites.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { PokeSubscriber } from '../../classes/poke-subscriber';
 import { PokeService } from '../../services/poke-service';
 
@@ -11,18 +11,25 @@ import { PokeService } from '../../services/poke-service';
 })
 export class PokeSprites extends PokeSubscriber {
 
-  private spriteData: Array<string> = []
+  /*
+    Don't forget to save your data in a signal: you will run into errors if you don't
+  */
+  spriteData: WritableSignal<Array<string>> = signal([]);
+
+  altText: string = "spriteImage"
 
   constructor(private pokeService: PokeService){
     // super is used to pass any necessary data into a parent class constructor
     super()
     this.subscription = this.pokeService.getPokemonSubject().subscribe( pokeData => {
-      this.spriteData = []; // make sure to clear out old sprite links
-      // for (var in obj) is used to iterate through object keys, for (var of arr) is used for arrays
-      for(const link of Object.values(pokeData.sprites)){
-        this.spriteData.push(link);
-      }
-      console.log(this.spriteData);
+      /*
+        Because the signal stores an array of strings we set the elements inside of our array and
+        then use the set function for this signal to update the stored array
+      */
+      const newSpriteData = [];
+      newSpriteData.push(pokeData.sprites.back_default);
+      newSpriteData.push(pokeData.sprites.front_default);
+      this.spriteData.set(newSpriteData);
     });
   }
 
