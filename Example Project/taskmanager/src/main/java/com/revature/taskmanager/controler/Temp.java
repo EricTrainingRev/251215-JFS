037068ffcf52641567d3e0f80516912083bc6c68
EricTrainingRev <eric.suminski@revature.com>
2025-12-26 11:02:17 -0500

    adding initial JWT implementation to example project

--- Example Project/taskmanager/src/main/java/com/revature/taskmanager/controler/Temp.java
+++ Example Project/taskmanager/src/main/java/com/revature/taskmanager/controler/Temp.java
@@ -1,9 +1,12 @@
 package com.revature.taskmanager.controler;
 
+import com.revature.taskmanager.entity.Admin;
 import com.revature.taskmanager.entity.Client;
 import com.revature.taskmanager.entity.Task;
+import com.revature.taskmanager.repository.AdminRepository;
 import com.revature.taskmanager.repository.ClientRepository;
 import com.revature.taskmanager.repository.TaskRepository;
+import com.revature.taskmanager.util.JwtUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -19,9 +22,28 @@ import java.util.List;
 public class Temp {
 
     @Autowired
+    private JwtUtility jwtUtility;
+    @Autowired
+    private AdminRepository adminRepository;
+    @Autowired
     private ClientRepository clientRepository;
     @Autowired
     private TaskRepository taskRepository;
+
+    @PostMapping("/admin")
+    public Admin createAdmin(@RequestBody Admin admin){
+        return adminRepository.save(admin);
+    }
+
+    @PostMapping("/login")
+    public TBD adminLogin(@RequestBody Admin admin){
+        // todo: implement login
+    }
+    
+    @GetMapping("/admin/thing")
+    public TBD adminAction(){
+        // TODO: get JWT token from request header and validate it
+    }
 
     @PostMapping("/client")
     public Client createClient(@RequestBody Client client){
